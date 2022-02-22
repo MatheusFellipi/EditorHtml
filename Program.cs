e15e1e8d444c4950860487931acb007f37e3e33c
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-02-22 15:42:28 -0300

    Editorhtml

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace EditorHtml
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Menu.Show();
     }
+  }
 }
